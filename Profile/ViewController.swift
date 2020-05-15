4d48140329c951f3aa3d7767ff0fa0d7a751c33f
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-15 20:19:10 +0900

    プロフィールアプリの完成

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var profileCommentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        profileImageView.isHidden = true
+        profileCommentLabel.isHidden = true
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func tapButton1() {
+        profileImageView.isHidden = false
+        profileCommentLabel.isHidden = false
+        
+    }
 
 }
 
